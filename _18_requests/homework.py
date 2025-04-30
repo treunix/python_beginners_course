d05c513fa9b3e115cbd600f66d05bbcac9b07923
treunix <cbvtyc1234@gmail.com>
2025-04-30 17:31:42 +0500

    домашка

---
+++ _18_requests/homework.py
@@ -0,0 +1,13 @@
+#  задание
+#  1. Отправьте на https://api.binance.com/api/v3/ticker/price запрос БЕЗ аргумента params.
+#  2. Изучите структуру ответа, сравните её с ответом в запросе без params.
+#  3. Напишите код, который найдёт курс Etherium'а к доллару (ETHUSDT) в полученной из запроса структуре.
+
+import requests
+
+response = requests.get('https://api.binance.com/api/v3/ticker/price')
+
+for ticker in response.json():
+    if ticker['symbol'] == 'ETHUSDT':
+        print(f'Курс ETH по USDT - {float(ticker['price'])}')
+        break
